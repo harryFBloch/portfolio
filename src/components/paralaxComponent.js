493ec0834c1794e43937a3152db0d20bcaae165d
harryFBloch <harryfbloch@gmail.com>
2019-03-25 13:17:33 -0400

    removed debugger

--- src/components/paralaxComponent.js
+++ src/components/paralaxComponent.js
@@ -3,10 +3,7 @@ import {Parallax, Row} from 'react-materialize'
 
 export default class ParalaxComponent extends Component {
 
-
-
   render(){
-    debugger
     return (
       <Row>
         <Parallax imageSrc="https://www.thoughtco.com/thmb/DF3Q0T5_0O5CmGBTCWCBTcyGgmw=/768x0/filters:no_upscale():max_bytes(150000):strip_icc()/JavaScript-58acbb8a3df78c345bad32c2.jpg"></Parallax>
