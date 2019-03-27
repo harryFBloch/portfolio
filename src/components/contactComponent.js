c0fe3bbfb39c171ed89df65da2ecf126efb24aa2
harryFBloch <harryfbloch@gmail.com>
2019-03-27 11:34:44 -0400

    fixed gitHub and linkedIn

--- src/components/contactComponent.js
+++ src/components/contactComponent.js
@@ -4,8 +4,8 @@ export default function ContactComponent(){
   return (
     <>
       <h2 id="Contact">Contact</h2>
-      <h5 className="center-align"><a href="https://github.com/harryFBloch">Github</a></h5>
-      <h5 className="center-align"><a href="https://www.linkedin.com/in/harry-bloch-bb6a977b/">Linkedin</a></h5>
+      <h5 className="center-align"><a href="https://github.com/harryFBloch">GitHub</a></h5>
+      <h5 className="center-align"><a href="https://www.linkedin.com/in/harry-bloch-bb6a977b/">LinkedIn</a></h5>
       <h5 className="center-align"><a href="mailto:harryfbloch@gmail.com?Subject=Hello%20World!" target="_top">HarryFBloch@Gmail.com</a></h5>
     </>
   )
