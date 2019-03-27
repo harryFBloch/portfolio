34ae3786c275d7db9d59ed643da71249519f0a38
harryFBloch <harryfbloch@gmail.com>
2019-03-27 12:19:24 -0400

    changed grammar in about me

--- src/components/aboutComponent.js
+++ src/components/aboutComponent.js
@@ -12,7 +12,7 @@ export default function AboutCompoent(){
       <Col s={6}>
         <h2 id="About">About Me</h2>
         <p>
-          Full Stack Developer with experience in iOS and Culinary. Studying cooking gave me considerable experience with organization and time management, these skills translate very well to writing code. I turned to coding when I made a simple Frogger-like game through a tutorial, and found a passion for logic I never knew I had. I have experience working with a team and understand the importance of writing clean, readable code. I am passionate about the Rangers, working out and learning new technologies.
+          Full Stack Developer with experience in iOS and Culinary. Studying cooking gave me considerable experience with organization and time management - two skills that translate very well to writing code. I turned to coding when I made a simple Frogger-like game through a tutorial, and found a passion for logic I never knew I had. I have experience working with a team and understand the importance of writing clean, readable code. I am passionate about the Rangers, working out and learning new technologies.
         </p>
       </Col>
     </Row>
